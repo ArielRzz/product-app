6d2f978cf7cba1b5f3b11c4cf537f3094bc2e429
ArielRzz <99253027+ArielRzz@users.noreply.github.com>
2023-05-04 15:45:01 -0300

    added Product entity

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,29 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PRODUCT_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "PRICE")
+    private Double price;
+
+    @Column(name = "QUANTITY")
+    private Integer quantity;
+}
